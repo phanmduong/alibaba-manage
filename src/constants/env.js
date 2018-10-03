e21277fcdb89a701751ca3e2da7c47b1eda68e6d
Phan Minh Dương <phanminhduong@hotmail.com>
2018-10-04 00:28:18 +0700

    update code

--- src/constants/env.js
+++ src/constants/env.js
@@ -1,9 +1,9 @@
 /**
  * Created by phanmduong on 4/5/17.
  */
-export const API_URL = "http://api.alibabaenglish.edu.vn";
-export const MANAGE_API_URL = "http://manageapi.alibabaenglish.edu.vn";
-export const BASE_URL = "http://alibabaenglish.edu.vn";
-export const API_NODE_URL = "http://alibabaenglish.edu.vn:8000";
-export const SOCKET_URL = "http://alibabaenglish.edu.vn:3000";
+export const API_URL = "http://api.keetool8.xyz";
+export const MANAGE_API_URL = "http://manageapi.keetool8.xyz";
+export const BASE_URL = "http://keetool8.xyz";
+export const API_NODE_URL = "http://keetool8.xyz:8000";
+export const SOCKET_URL = "http://keetool8.xyz:3000";
 export const SOCKET_CHANNEL = "alibaba-channel";
